89cbe026b62e01080105aad6146e202b385a06d8
Ldmurillo <luciadaniela22@gmail.com>
2023-01-11 18:18:30 -0300

    Primer componente

---
+++ 00-mi-primer-componente/src/components/custom-buttom.jsx
@@ -0,0 +1,3 @@
+export function CustomButton(text) {
+    return <button>{text.text}</button>;
+}
---
+++ 00-mi-primer-componente/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CustomButton } from './components/custom-buttom'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <CustomButton text="Click" />
+)
